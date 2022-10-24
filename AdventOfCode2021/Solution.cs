987580087edbdcc6c9c85435c58148d459d990cd
NIrk <nirk.nirkaz@gmail.com>
2022-10-24 21:06:47 +0300

    Sonar Sweep part 2

--- AdventOfCode2021/Solution.cs
+++ AdventOfCode2021/Solution.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -19,19 +20,18 @@ namespace AdventOfCode2021
         //https://adventofcode.com/2021/day/1
         public static int D1SonarSweep()
         {
-            /*Count the number of times a depth measurement increases from the previous measurement. 
-             * (There is no measurement before the first measurement.)*/
             var count = 0;
-            var input = GetPuzzleInput("PuzzleInputD1.txt");
-            var prevValue = int.Parse(input[0]);
+            var inputRaw = Array.ConvertAll(GetPuzzleInput("PuzzleInputD1.txt"), s => int.Parse(s));
+            var prevValue = int.MaxValue;
 
-            for (var i = 1; i < input.Length; i++)
+            for (var i = 0; i < inputRaw.Length - 2; i++)
             {
-                if (int.Parse(input[i]) > prevValue) count++;
-                prevValue = int.Parse(input[i]);
+                var sum = inputRaw[i] + inputRaw[i+1] + inputRaw[i+2];
+                if (sum > prevValue) count++;
+                prevValue = sum;
             }
 
-            return count;
+            return count; // 514
         }
     }
 }
