8212d8f6a2782fc3a9ba8f456eda4cdd14317756
NIrk <nirk.nirkaz@gmail.com>
2022-10-24 20:30:20 +0300

    Add project files.

---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode2021;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.D1SonarSweep());
+    }
+}
---
+++ AdventOfCode2021/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    internal static class Solution
+    {
+        private static string[] GetPuzzleInput(string fileName)
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @$"Input\{fileName}");
+            return File.ReadAllLines(path);
+        }
+
+        //https://adventofcode.com/2021/day/1
+        public static int D1SonarSweep()
+        {
+            var res = 0;
+            var input = GetPuzzleInput("PuzzleInputD1.txt");
+
+
+            return res;
+        }
+    }
+}
